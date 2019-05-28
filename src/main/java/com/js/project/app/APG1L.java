a17697be58b41d51bea7af20d8c4475928c058b9
JolitaSa <jolita.sapaliene@gmail.com>
2019-05-28 16:06:34 +0300

    init commit

---
+++ src/main/java/com/js/project/app/APG1L.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.js.project.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jolita
+ */
+public class APG1L extends Stocks {
+
+    APG1L apr = new APG1L();
+    List<Double> pLast = new ArrayList<>();
+   
+//    pLast.add (1.615);
+//    pLast.add (1.610);
+//    pLast.add (1.605);
+//    pLast.add (1.605);
+//    pLast.add (1.605);     
+
+    }
---
+++ src/main/java/com/js/project/app/ARC1T.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.js.project.app;
+
+/**
+ *
+ * @author Jolita
+ */
+public class ARC1T extends Stocks {
+
+    
+
+}
